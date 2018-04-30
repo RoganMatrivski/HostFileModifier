0074f8f60ab92fc36042015646a8ba995ba33dd7
Rogan Matrivski <robinmauritzrm@gmail.com>
2018-05-01 04:28:37 +0700

    Licensing Stuff

--- Host File Modifier/Program.cs
+++ Host File Modifier/Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+/*
+A Program to Patch "hosts" File.
+Copyright (C) 2018  Rogan Matrivski Lartengalf
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Net;
@@ -180,7 +198,7 @@ namespace Host_File_Modifier
             int readPosition = 0 - 1;
             int startEditing = 0, endEditing = 0;
             if (originalHosts.Contains("#[HOST_EDIT_START]"))
-            { 
+            {
                 foreach (string host in originalHosts)
                 {
                     readPosition++;
